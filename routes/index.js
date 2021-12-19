fc6dce0282bca4f5a52dd3609e0d6fae537b3941
leminhduc201299 <duc.lm173043@sis.hust.edu.vn>
2021-12-19 17:01:50 +0700

    the first deploy on heroku

--- routes/index.js
+++ routes/index.js
@@ -3,88 +3,119 @@ var router = express.Router();
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+const wiki = require('wikijs').default;
+
+// Parse a SPARQL query to a JSON object
+var SparqlParser = require('sparqljs').Parser;
+var parser = new SparqlParser();
+
+var parsedQuery = parser.parse(
+    'PREFIX foaf: <http://xmlns.com/foaf/0.1/> ' +
+    'SELECT * { ?mickey foaf:name "Mickey Mouse"@en; foaf:knows ?other. }');
+
+
+
 /* GET home page. */
 router.post('/chatbot', function (req, res, next) {
     (async () => {
-        let url = req.body.queryResult.parameters.url;
-        if (url) {
-            try {
-                const response = await axios.get(url);
 
-                const $ = cheerio.load(response.data);
+        let page = await wiki()
+            .page('messi')
+        
+        let pageInfo = await page.fullInfo();
+        let pageSummary = await page.summary();
 
-                /* Get product title */
-                let queryTitle = $('#productTitle');
-                let title;
-                if (queryTitle) {
-                    title = queryTitle.text();
+        console.log(pageInfo, pageSummary)
+
+        return res.json({
+            fulfillmentText: "fulfillmentText",
+            fulfillmentMessages: [{
+                "text": {
+                    "text": ["Error"]
                 }
+            }],
+            source: "webhook-sample"
+        });
 
-                /* Get availability */
-                let queryAvailability = $('span', '#availability');
-                let formattedAvailability;
-                if (queryAvailability) {
-                    formattedAvailability = queryAvailability.text();
-                }
+        // let url = req.body.queryResult.parameters.url;
+        // if (url) {
+        //     try {
+        //         const response = await axios.get(url);
 
-                /* Get price */
-                let queryPrice = $('#priceblock_ourprice');
-                let price;
-                if (queryPrice) {
-                    price = queryPrice.text();
-                }
+        //         const $ = cheerio.load(response.data);
 
-                /* Get total price */
-                let totalCost;
-                let queryTotalCost = $('b', '#amazonGlobal_feature_div');
-                if (queryTotalCost) {
-                    totalCost = queryTotalCost.text();
-                }
+        //         /* Get product title */
+        //         let queryTitle = $('#productTitle');
+        //         let title;
+        //         if (queryTitle) {
+        //             title = queryTitle.text();
+        //         }
 
-                /* Get attribute */
-                let formattedAttribute = [];
-                let queryAttribute = $('span', '#productOverview_feature_div');
-                if (queryAttribute) {
-                    queryAttribute.each(function (i, elem) {
-                        // Range Name
-                        // console.log("iteration - ", i);
-                        // console.log("name - ", $(this).text().trim());
-                        formattedAttribute[i] = $(this).text().trim();
-                    });
-                }
+        //         /* Get availability */
+        //         let queryAvailability = $('span', '#availability');
+        //         let formattedAvailability;
+        //         if (queryAvailability) {
+        //             formattedAvailability = queryAvailability.text();
+        //         }
 
-                return res.json({
-                    fulfillmentText: "fulfillmentText",
-                    fulfillmentMessages: [{
-                        "text": {
-                            "text": [{
-                                "title": title,
-                                // "rating": parsedRating,
-                                "availability": formattedAvailability,
-                                "price": price,
-                                "totalCost": totalCost,
-                                "formattedAttribute": formattedAttribute,
-                                // "features": formattedFeatures,
-                            }]
-                        }
-                    }],
-                    source: "webhook-sample"
-                });
-            }
-            catch (error) {
-                console.log(error);
-                return res.json({
-                    fulfillmentText: "fulfillmentText",
-                    fulfillmentMessages: [{
-                        "text": {
-                            "text": ["Error"]
-                        }
-                    }],
-                    source: "webhook-sample"
-                });
-            }
+        //         /* Get price */
+        //         let queryPrice = $('#priceblock_ourprice');
+        //         let price;
+        //         if (queryPrice) {
+        //             price = queryPrice.text();
+        //         }
 
-        }
+        //         /* Get total price */
+        //         let totalCost;
+        //         let queryTotalCost = $('b', '#amazonGlobal_feature_div');
+        //         if (queryTotalCost) {
+        //             totalCost = queryTotalCost.text();
+        //         }
+
+        //         /* Get attribute */
+        //         let formattedAttribute = [];
+        //         let queryAttribute = $('span', '#productOverview_feature_div');
+        //         if (queryAttribute) {
+        //             queryAttribute.each(function (i, elem) {
+        //                 // Range Name
+        //                 // console.log("iteration - ", i);
+        //                 // console.log("name - ", $(this).text().trim());
+        //                 formattedAttribute[i] = $(this).text().trim();
+        //             });
+        //         }
+
+        //         return res.json({
+        //             fulfillmentText: "fulfillmentText",
+        //             fulfillmentMessages: [{
+        //                 "text": {
+        //                     "text": [{
+        //                         "title": title,
+        //                         // "rating": parsedRating,
+        //                         "availability": formattedAvailability,
+        //                         "price": price,
+        //                         "totalCost": totalCost,
+        //                         "formattedAttribute": formattedAttribute,
+        //                         // "features": formattedFeatures,
+        //                     }]
+        //                 }
+        //             }],
+        //             source: "webhook-sample"
+        //         });
+        //     }
+        //     catch (error) {
+        //         console.log(error);
+        //         return res.json({
+        //             fulfillmentText: "fulfillmentText",
+        //             fulfillmentMessages: [{
+        //                 "text": {
+        //                     "text": ["Error"]
+        //                 }
+        //             }],
+        //             source: "webhook-sample"
+        //         });
+        //     }
+
+        // }
     })();
 
 });
