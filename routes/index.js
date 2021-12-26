4fad0e84f9c716d2bcd2d521d721b23cde11a236
leminhduc201299 <duc.lm173043@sis.hust.edu.vn>
2021-12-26 09:31:30 +0700

    add res

--- routes/index.js
+++ routes/index.js
@@ -36,101 +36,38 @@ router.post('/chatbot', function (req, res, next) {
         }
 
         let page = await wiki().page(person.name)
-
         let pageInfo = await page.fullInfo();
         let pageSummary = await page.summary();
 
-        console.log('******', pageInfo, pageSummary)
+        if (action === ask_age) {
+            let age = pageInfo.general.birthDate.age;
 
-        return res.json({
-            fulfillmentText: "fulfillmentText",
-            fulfillmentMessages: [{
-                "text": {
-                    "text": ["I didn't get that. Can you repeat?"]
-                }
-            }],
-            source: "webhook-sample"
-        });
+            let textRes = `He is ${age} years old`;
 
-        // let url = req.body.queryResult.parameters.url;
-        // if (url) {
-        //     try {
-        //         const response = await axios.get(url);
+            return res.json({
+                fulfillmentText: "fulfillmentText",
+                fulfillmentMessages: [{
+                    "text": {
+                        "text": [textRes]
+                    }
+                }],
+                source: "webhook-sample"
+            });
+        }
+        else {
+            return res.json({
+                fulfillmentText: "fulfillmentText",
+                fulfillmentMessages: [{
+                    "text": {
+                        "text": ["I didn't get that. Can you repeat?"]
+                    }
+                }],
+                source: "webhook-sample"
+            });
+        }
 
-        //         const $ = cheerio.load(response.data);
+        
 
-        //         /* Get product title */
-        //         let queryTitle = $('#productTitle');
-        //         let title;
-        //         if (queryTitle) {
-        //             title = queryTitle.text();
-        //         }
-
-        //         /* Get availability */
-        //         let queryAvailability = $('span', '#availability');
-        //         let formattedAvailability;
-        //         if (queryAvailability) {
-        //             formattedAvailability = queryAvailability.text();
-        //         }
-
-        //         /* Get price */
-        //         let queryPrice = $('#priceblock_ourprice');
-        //         let price;
-        //         if (queryPrice) {
-        //             price = queryPrice.text();
-        //         }
-
-        //         /* Get total price */
-        //         let totalCost;
-        //         let queryTotalCost = $('b', '#amazonGlobal_feature_div');
-        //         if (queryTotalCost) {
-        //             totalCost = queryTotalCost.text();
-        //         }
-
-        //         /* Get attribute */
-        //         let formattedAttribute = [];
-        //         let queryAttribute = $('span', '#productOverview_feature_div');
-        //         if (queryAttribute) {
-        //             queryAttribute.each(function (i, elem) {
-        //                 // Range Name
-        //                 // console.log("iteration - ", i);
-        //                 // console.log("name - ", $(this).text().trim());
-        //                 formattedAttribute[i] = $(this).text().trim();
-        //             });
-        //         }
-
-        //         return res.json({
-        //             fulfillmentText: "fulfillmentText",
-        //             fulfillmentMessages: [{
-        //                 "text": {
-        //                     "text": [{
-        //                         "title": title,
-        //                         // "rating": parsedRating,
-        //                         "availability": formattedAvailability,
-        //                         "price": price,
-        //                         "totalCost": totalCost,
-        //                         "formattedAttribute": formattedAttribute,
-        //                         // "features": formattedFeatures,
-        //                     }]
-        //                 }
-        //             }],
-        //             source: "webhook-sample"
-        //         });
-        //     }
-        //     catch (error) {
-        //         console.log(error);
-        //         return res.json({
-        //             fulfillmentText: "fulfillmentText",
-        //             fulfillmentMessages: [{
-        //                 "text": {
-        //                     "text": ["Error"]
-        //                 }
-        //             }],
-        //             source: "webhook-sample"
-        //         });
-        //     }
-
-        // }
     })();
 
 });
