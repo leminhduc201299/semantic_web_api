26c826c7db03d14692d01bb47016417e452f6094
leminhduc201299 <duc.lm173043@sis.hust.edu.vn>
2022-01-10 16:49:42 +0700

    add sparql

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,39 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios');
 const Format = require('../utils/format.js')
+const sendQuery = require('../utils/sendQuery.js')
 
 const wiki = require('wikijs').default;
+
+const newEngine = require('@comunica/actor-init-sparql').newEngine;
+
+const myEngine = newEngine();
+
+
+let option = {
+    "default-graph-uri": "http://dbpedia.org",
+    "format": "application/sparql-results+json",
+    "query": 'SELECT ?x ?height WHERE {?x <http://dbpedia.org/property/name> ?name. ?x <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://dbpedia.org/ontology/Person>. ?x <http://dbpedia.org/ontology/birthPlace> ?height. FILTER regex(?name, "Công Phượng", "i")}',
+    "signal_unconnected": "on",
+    "signal_void": "on",
+    "timeout": "30000"
+}
 
 /* GET home page. */
 router.post('/chatbot', function (req, res, next) {
     (async () => {
+        // console.log(`https://dbpedia.org/sparql?${queryString(option)}`)
+        // axios({
+        //     url: `https://dbpedia.org/sparql?${queryString(option)}`,
+        //     method: "GET",
+        // })
+        // .then((res) => {
+        //     console.log('res: ', res.data.results.bindings)
+        // })
+        // .catch((error) => {
+        //     console.log(error)
+        // });
+
         if (!(req.body && req.body.queryResult && req.body.queryResult.parameters)) {
             return res.json({
                 fulfillmentText: "fulfillmentText",
@@ -23,7 +50,6 @@ router.post('/chatbot', function (req, res, next) {
         let parameters = req.body.queryResult.parameters;
         let person = parameters.person;
         let action = req.body.queryResult.action;
-
         if (!(person && action)) {
             return res.json({
                 fulfillmentText: "fulfillmentText",
@@ -36,101 +62,112 @@ router.post('/chatbot', function (req, res, next) {
             });
         }
 
-        let page;
-        try {
-            page = await wiki().page(person.name);
-        } catch (error) {
-            console.log(error)
-        }
-        
-        if (!page) {
-            return res.json({
-                fulfillmentText: "fulfillmentText",
-                fulfillmentMessages: [{
-                    "text": {
-                        "text": ["I missed that, say that again?"]
-                    }
-                }],
-                source: "webhook-sample"
-            });
-        }
-
-        let pageInfo = await page.fullInfo();
-        let textRes = '';
-        console.log('pageInfo: ', pageInfo);
-        
+        /**
+         * Query Dbpedia
+         */
+        let textRes = '';        
         if (action === 'ask_summary') {
-            let pageSummary = await page.summary();
+            option.query = `SELECT ?x ?abstract WHERE {?x <http://dbpedia.org/property/name> ?name. ?x <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://dbpedia.org/ontology/SoccerPlayer>. ?x <http://dbpedia.org/ontology/abstract> ?abstract. FILTER(regex(?name, "${person.name}", "i") && lang(?abstract)="en")}`;
 
-            if (pageSummary) {
-                textRes = pageSummary;
+            let result = await sendQuery(option);
+
+            if (result.isValid) {
+                textRes = result.data[0].abstract.value;
             } else {
-                textRes = "I didn't get that. Can you repeat?"
+                textRes = result.textRes;
+            }
+        }
+        else if (action === 'ask_height') {
+            option.query = `SELECT ?x ?height WHERE {?x <http://dbpedia.org/property/name> ?name. ?x <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://dbpedia.org/ontology/SoccerPlayer>. ?x <http://dbpedia.org/ontology/height> ?height. FILTER(regex(?name, "${person.name}", "i"))}`;
+
+            let result = await sendQuery(option);
+
+            if (result.isValid) {
+                let height = result.data[0].height.value;
+                textRes = `${person.name}'s height is ${height}m`;
+            } else {
+                textRes = result.textRes;
             }
         }
         else if (action === 'ask_age') {
-            if (!(pageInfo && pageInfo.general && pageInfo.general.birthDate && pageInfo.general.birthDate.age)) {
-                textRes = "I didn't get that. Can you repeat?"
-            }
-            else {
-                let age = pageInfo.general.birthDate.age;
+            option.query = `SELECT ?x ?birthDate WHERE {?x <http://dbpedia.org/property/name> ?name. ?x <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://dbpedia.org/ontology/SoccerPlayer>. ?x <http://dbpedia.org/ontology/birthDate> ?birthDate. FILTER(regex(?name, "${person.name}", "i"))}`;
+
+            let result = await sendQuery(option);
+
+            if (result.isValid) {
+                let birthday = result.data[0].birthDate.value;
+                let ageDifMs = Date.now() - new Date(birthday).getTime();
+                let ageDate = new Date(ageDifMs); // miliseconds from epoch
+                let age = Math.abs(ageDate.getUTCFullYear() - 1970);
+
                 textRes = `${person.name} is ${age} years old`;
+            } else {
+                textRes = result.textRes;
             }
         }
         else if (action === 'ask_birthday') {
-            if (!(pageInfo && pageInfo.general && pageInfo.general.birthDate && pageInfo.general.birthDate.date)) {
-                textRes = "I didn't get that. Can you repeat?"
-            }
-            else {
-                let birthday = pageInfo.general.birthDate.date;
+            option.query = `SELECT ?x ?birthDate WHERE {?x <http://dbpedia.org/property/name> ?name. ?x <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://dbpedia.org/ontology/SoccerPlayer>. ?x <http://dbpedia.org/ontology/birthDate> ?birthDate. FILTER(regex(?name, "${person.name}", "i"))}`;
+
+            let result = await sendQuery(option);
+
+            if (result.isValid) {
+                let birthday = result.data[0].birthDate.value;
                 textRes = `${person.name}'s birthday is  ${Format.formatDate(birthday)}`;
+            } else {
+                textRes = result.textRes;
             }
         }
-        else if (action === 'ask_birthplace') {
-            if (!(pageInfo && pageInfo.general && pageInfo.general.birthPlace)) {
-                textRes = "I didn't get that. Can you repeat?"
-            }
-            else {
-                let birthPlace = pageInfo.general.birthPlace;
-                textRes = `${person.name} was born in  ${birthPlace}`;
-            }
-        }
-        else if (action === 'ask_currentteam') {
-            if (!(pageInfo && pageInfo.general && pageInfo.general.currentclub)) {
-                textRes = "I didn't get that. Can you repeat?"
-            }
-            else {
-                let currentclub = pageInfo.general.currentclub;
-                textRes = `${person.name} is playing for ${currentclub}`;
-            }
-        }
+        // else if (action === 'ask_birthplace') {
+        //     if (!(pageInfo && pageInfo.general && pageInfo.general.birthPlace)) {
+        //         textRes = "I didn't get that. Can you repeat?"
+        //     }
+        //     else {
+        //         let birthPlace = pageInfo.general.birthPlace;
+        //         textRes = `${person.name} was born in  ${birthPlace}`;
+        //     }
+        // }
+        // else if (action === 'ask_currentteam') {
+        //     if (!(pageInfo && pageInfo.general && pageInfo.general.currentclub)) {
+        //         textRes = "I didn't get that. Can you repeat?"
+        //     }
+        //     else {
+        //         let currentclub = pageInfo.general.currentclub;
+        //         textRes = `${person.name} is playing for ${currentclub}`;
+        //     }
+        // }
         else if (action === 'ask_fullname') {
-            if (!(pageInfo && pageInfo.general && pageInfo.general.fullname)) {
-                textRes = "I didn't get that. Can you repeat?"
-            }
-            else {
-                let fullname = pageInfo.general.fullname;
-                textRes = `Full name is ${fullname}`;
+            option.query = `SELECT ?x ?fullname WHERE {?x <http://dbpedia.org/property/name> ?name. ?x <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://dbpedia.org/ontology/SoccerPlayer>. ?x <http://dbpedia.org/property/fullname> ?fullname. FILTER(regex(?name, "${person.name}", "i"))}`;
+
+            let result = await sendQuery(option);
+
+            if (result.isValid) {
+                let fullname = result.data[0].fullname.value;
+                textRes = `${person.name}'s full name is ${fullname}`;
+            } else {
+                textRes = result.textRes;
             }
         }
         else if (action === 'ask_number') {
-            if (!(pageInfo && pageInfo.general && pageInfo.general.clubnumber)) {
-                textRes = "I didn't get that. Can you repeat?"
-            }
-            else {
-                let clubnumber = pageInfo.general.clubnumber;
-                textRes = `${person.name}'s club number is ${clubnumber}`;
-            }
-        }
-        else if (action === 'ask_position') {
-            if (!(pageInfo && pageInfo.general && pageInfo.general.position)) {
-                textRes = "I didn't get that. Can you repeat?"
-            }
-            else {
-                let position = pageInfo.general.position;
-                textRes = `${person.name} plays the position of  ${position.constructor === Array ? position.join(', ') : position}`;
+            option.query = `SELECT ?x ?number WHERE {?x <http://dbpedia.org/property/name> ?name. ?x <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://dbpedia.org/ontology/SoccerPlayer>. ?x <http://dbpedia.org/ontology/number> ?number. FILTER(regex(?name, "${person.name}", "i"))}`;
+
+            let result = await sendQuery(option);
+
+            if (result.isValid) {
+                let number = result.data[0].number.value;
+                textRes = `${person.name}'s club number is ${number}`;
+            } else {
+                textRes = result.textRes;
             }
         }
+        // else if (action === 'ask_position') {
+        //     if (!(pageInfo && pageInfo.general && pageInfo.general.position)) {
+        //         textRes = "I didn't get that. Can you repeat?"
+        //     }
+        //     else {
+        //         let position = pageInfo.general.position;
+        //         textRes = `${person.name} plays the position of  ${position.constructor === Array ? position.join(', ') : position}`;
+        //     }
+        // }
         else {
             textRes = "I didn't get that. Can you repeat?"
         }
---
+++ utils/sendQuery.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+const queryString = require('./queryString.js')
+
+var sendQuery = async function (option) {
+    let res = await axios({
+        url: `https://dbpedia.org/sparql?${queryString(option)}`,
+        method: "GET",
+    })
+    let data = res.data.results.bindings;
+    console.log(data)
+
+    let result = {
+        isValid: true,
+        textRes: '',
+        data: null
+    }
+
+    if (!(data && data.length)) {
+        result.isValid = false;
+        result.textRes = "The football player you requested could not be found";
+        return result;
+    } else if (data.length > 1) {
+        result.isValid = false;
+        result.textRes = "Please enter the full name of the football player you want to search for"
+        return result;
+    } else {
+        result.data = data;
+        return result;
+    }
+}
+
+module.exports = sendQuery;
